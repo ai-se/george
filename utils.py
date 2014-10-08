5fbf46237f91b7091c69daf9bebacc428168232b
GeorgePanzerMathew <george.meg91@gmail.com>
2014-10-08 19:26:42 -0400

    Logic for interpolation added

---
+++ utils.py
@@ -0,0 +1,13 @@
+INDEP = [ 
+         # 0..8
+         'Prec', 'Flex', 'Resl', 'Team', 'Pmat', 'rely', 'data', 'cplx', 'ruse',
+         # 9 .. 17
+         'docu', 'time', 'stor', 'pvol', 'acap', 'pcap', 'pcon', 'aexp', 'plex',  
+         # 18 .. 25
+         'ltex', 'tool', 'site', 'sced', 'kloc']
+
+LESS = ['effort', 'defects', 'months']
+
+DATATYPES = [int for x in range(22)] + [float for x in range(4)]
+ 
+
