ef21252ce0555dc04215f15b688cef3c815044ee
GeorgePanzerMathew <george.meg91@gmail.com>
2014-11-12 14:25:59 -0500

    Included CART to measure error

--- testRig.py
+++ testRig.py
@@ -1,5 +1,7 @@
 from __future__ import division,print_function
 import sys, random, math
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier 
 from lib import *
 from where2 import *
 from interpolation import *
@@ -22,21 +24,31 @@ def say(*lst):
   print(*lst,end="")
   sys.stdout.flush()
 
+def formatForCART(test,trains,
+        indep=lambda x: x.cells[:-3]
+        ,dep=lambda x: x.cells[-3]):
+  trainInputSet = []
+  trainOutputSet = []
+  for train in trains:
+    trainInputSet+=[indep(train)]
+    trainOutputSet+=[dep(train)]
+  return trainInputSet, trainOutputSet, indep(test), dep(test)
 
 def testRig(dataset=nasa93(), duplicator=interpolateNTimes):
   def effort(row):
       return row.cells[-3]
-  scores=dict(clusterk1=N(),knn=N())
+  scores=dict(clusterk1=N(),knn=N(),CART=N())
   for score in scores.values():
-    score.go=True
+    score.go=False
+  scores['CART'].go = True
   for test, train in loo(dataset._rows):
     say(".")
     desired_effort = effort(test)
-    duplicatedModel = duplicator(dataset, train, 1) 
+    duplicatedModel = duplicator(dataset, train, 6) 
     tree = launchWhere2(duplicatedModel, rows=None, verbose=False)
     """
     Selecting the closest cluster and the closest row
-    """
+    """ 
     def clusterk1(score):
       test_leaf = leaf(duplicatedModel, test, tree)
       nearest_row = closest(duplicatedModel, test, test_leaf.val)
@@ -52,10 +64,20 @@ def testRig(dataset=nasa93(), duplicator=interpolateNTimes):
       expectedSum = sum(map(lambda x:effort(x[1]), nearestN))
       test_effort = expectedSum / k
       score += abs(desired_effort - test_effort)/desired_effort
+    
+    def CART(score):
+      trainIp, trainOp, testIp, testOp = formatForCART(test,duplicatedModel._rows);
+      decTree = DecisionTreeClassifier(criterion="entropy", random_state=1)
+      decTree.fit(trainIp,trainOp)
+      test_effort = decTree.predict(testIp)
+      score += abs(desired_effort - test_effort)/desired_effort
+
     n = scores["clusterk1"]
     n.go and clusterk1(n)
     n = scores["knn"]
     n.go and kNearestNeighbor(n, k=1)
+    n = scores["CART"]
+    n.go and CART(score)
   #print(scores["clusterk1"])
   print("")
   for key,n in scores.items():
